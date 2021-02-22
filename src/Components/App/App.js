bfbfbe318139636536de2a5288310682a4187e6d
Anneke Schütte <a.schuette.hh@gmail.com>
2021-02-22 17:52:01 +0100

    Add score keeper functions

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,69 @@
 import React from 'react';
+import { useState } from 'react'
 import './App.css';
 import PlayerForm from '../PlayerForm/PlayerForm';
 import Button from '../Button/Button';
 import Player from '../Player/Player';
 
-export default function App() {
+function App() {
+
+  const [players, setPlayers] = useState([])
+
+  function handleAddPlayer(name){
+    setPlayers(oldPlayers => [...oldPlayers, {name, score: 0}])
+  }
+
+  function handlePlus(index){
+    setPlayers(players => [
+      ...players.slice(0, index),
+      { ...players[index], score: players[index].score + 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
+  function handleMinus(index){
+    setPlayers(players => [
+      ...players.slice(0, index),
+      { ...players[index], score: players[index].score - 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
+  function resetAll(){
+    setPlayers([])
+  }
+
+  function resetScore(name){
+    setPlayers(players.map(player => ({ ...player, score: 0 })))
+  }
+
   return (
     <div className="App">
       <header className="AppHeader">Score Keeper</header>
       <main className="AppMain">
         <PlayerForm
-        onAddPlayer={(e) => {
-          e.preventDefault();
-          console.log('Form submitted');
-          }} />
+          onAddPlayer={handleAddPlayer}
+        />
 
-          <Player name={"John Doe"} score={20} onPlus={() => console.log('Plus!')} onMinus={() => console.log('Minus!')} />
+        {players.map((player, index) =>
+          <Player
+            name={player.name}
+            score={player.score}
+            onPlus={() => handlePlus(index)}
+            onMinus={() => handleMinus(index)}
+          />
+        )}
 
-          <Player name={"Jane Doe"} score={30} onPlus={() => console.log('Plus!')} onMinus={() => console.log('Minus!')} />
-
-          <Button name={"Reset Scores"} />
-          <Button name={"Reset All"} />
-        </main>
+        <Button
+          onClick={resetScore}
+          name={"Reset Scores"}
+        />
+        <Button
+          onClick={resetAll}
+          name={"Reset All"}
+        />
+      </main>
     </div>
   )
 }
+export default App
