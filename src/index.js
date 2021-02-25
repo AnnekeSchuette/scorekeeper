c9dd643de459d2f8b0cf55579ceaac7fc214c6f3
Anneke Schütte <a.schuette.hh@gmail.com>
2021-02-25 11:55:48 +0100

    Import and initialize Router in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route
+} from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
